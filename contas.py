b67ceac9a613ee5b1fe315d593f01fceebde9c6d
Enzo Kiyoshi <sampaioenzo000@gmail.com>
2024-05-27 08:45:21 -0300

    Add files via upload

---
+++ contas.py
@@ -0,0 +1,12 @@
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+
+soma = num1 + num2
+menos = num1 - num2
+vezes = num1 * num2
+divisao = num1 / num2
+
+print('A soma dos números é: ', soma)
+print('A subtração dos números é: ', menos)
+print('A multiplicação dos números é: ', vezes)
+print('A divisão dos números é: ', divisao)
